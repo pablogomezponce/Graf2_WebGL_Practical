547fcaf92644fbe45c74db6ff2786a0544044cd8
Pablo Gómez <pgomezponce@gmail.com>
2019-04-30 13:25:03 +0200

    final work

--- resources/js/scene.js
+++ resources/js/scene.js
@@ -2,6 +2,8 @@ var renderer;
 var scene;
 var camera;
 
+var cameraControl;
+
 function createRenderer() {
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0x000000,1.0);
@@ -15,11 +17,12 @@ function createCamera(){
         window.innerWidth / window.innerHeight,
         0.1, 1000);
         
-    camera.position.x = 15;
-    camera.position.y = 16;
-    camera.position.z = 13;
+    camera.position.x = 90;
+    camera.position.y = 32;
+    camera.position.z = 32;
     camera.lookAt(scene.position);
     
+    cameraControl = new THREE.OrbitControls(camera);
 }
 
 function init() {
@@ -29,7 +32,10 @@ function init() {
 
     createBox();
     createPlane();
+    createEarth(createEarthMaterial(),1,'earth');
+    createEarth(createFairClouds(),1.01,'clouds');
     createLight();
+    createEnviroment();
 
     document.body.appendChild(renderer.domElement);
 
@@ -38,6 +44,11 @@ function init() {
 
 function render() {
     renderer.render(scene,camera);
+
+cameraControl.update();
+    scene.getObjectByName('earth').rotation.y += 0.005;
+    scene.getObjectByName('clouds').rotation.y += 0.01;
+    
     requestAnimationFrame(render);
 }
 
@@ -75,6 +86,73 @@ function createLight() {
         spotLight.shadow.camera.far = 50;
     spotLight.castShadow = true;
     scene.add(spotLight);
+
+    var directionalLight = new THREE.DirectionalLight(0xffffff,1);
+
+    directionalLight.position.set(100,10, -50);
+    directionalLight.name = 'directional';
+    scene.add(directionalLight);
+    
+    var ambientLight = new THREE.AmbientLight(0x111111);
+    scene.add(ambientLight);
+}
+
+function createFairClouds() {
+    var cloudsTexture = new THREE.Texture();
+    var loader = new THREE.ImageLoader();
+    loader.load('assets/fair_clouds_1k.png', function (image){
+        cloudsTexture.image = image;
+        cloudsTexture.needsUpdate = true;
+    });
+    var fairCloudsMaterial = new THREE.MeshBasicMaterial();
+    fairCloudsMaterial.transparent = true;
+    fairCloudsMaterial.map = cloudsTexture;
+    return fairCloudsMaterial;
+}
+
+function createEarthMaterial() {
+    var earthTexture = new THREE.Texture();
+    var loader = new THREE.ImageLoader();
+    loader.load('assets/earthmap2k.jpg', function (image) {
+        earthTexture.image = image;
+        earthTexture.needsUpdate = true;
+    });
+
+
+
+    var earthMaterial = new THREE.MeshPhongMaterial();
+    earthMaterial.map = earthTexture;
+
+    loader.load('assets/earth_normalmap_flat2k.jpg', function (image) {
+        earthMaterial.normalMap = image;
+    });
+    earthMaterial.normalScale = new THREE.Vector2(1.0,1.0);
+
+    loader.load('assets/lee_spec.jpg', function (image) {
+        earthMaterial.specularMap = image;
+    });
+    earthMaterial.normalScale = new THREE.Color(0x262626);
+
+    return earthMaterial;
+}
+
+function createEarth(material,scale,name) {
+    var sphereGeometry = new THREE.SphereGeometry(15* scale,30 * scale,30 * scale);
+    var sphereMaterial = material;
+    var earthMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    earthMesh.name = name;
+    scene.add(earthMesh);
+}
+
+function createEnviroment() {
+    var envGeometry = new THREE.SphereGeometry(90,32,32);
+
+    var envMaterial = new THREE.MeshBasicMaterial();
+    envMaterial.map = THREE.ImageUtils.loadTexture('assets/galaxy_starfield.png');
+    envMaterial.side = THREE.BackSide;
+
+    var mesh = new THREE.Mesh(envGeometry, envMaterial);
+    scene.add(mesh);
 }
 
 init();
